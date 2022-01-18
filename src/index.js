12d2117cc5a9eff8071a4fa45a94c197333e1686
ginohmk <michotall95@gmail.com>
2022-01-18 08:38:19 +0100

    Add Code for review

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import './index.css';
+// eslint-disable-next-line import/no-cycle
+import TodoList, { inputTodo } from './module/updateStatus.js';
+import { editDescription, clearComoleted, render } from './module/AddRemove.js';
+
+const TodoListObj = new TodoList();
+
+export default function Starter() {
+  const threeDots = document.querySelectorAll('li');
+  threeDots.forEach((dotValue, index) => {
+    const dot = dotValue.querySelector('.three__dots');
+    if (dot) {
+      const newDot = dot.cloneNode(true);
+      dotValue.replaceChild(newDot, dot);
+      newDot.addEventListener('click', () => {
+        editDescription(dotValue, index, TodoListObj);
+        Starter();
+      });
+    }
+  });
+}
+
+inputTodo.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    TodoListObj.addTodo();
+    render(TodoListObj.list, TodoListObj);
+    Starter();
+  }
+});
+
+clearComoleted.addEventListener('click', () => {
+  TodoListObj.removeList();
+  render(TodoListObj.list, TodoListObj);
+  Starter();
+});
+
+render(TodoListObj.list, TodoListObj);
+Starter();
